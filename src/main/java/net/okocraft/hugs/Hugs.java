2ce6d2f10df0fbf48834e8ccb78bcc1d9ba413bc
Siroshun09 <siro.shun8@gmail.com>
2021-04-25 08:24:46 +0900

    perf(plugin): 非同期で処理するようにする

--- src/main/java/net/okocraft/hugs/Hugs.java
+++ src/main/java/net/okocraft/hugs/Hugs.java
@@ -16,13 +16,17 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Hugs extends JavaPlugin implements Listener {
 
     private final Map<Player, Long> lastHugTime = new HashMap<>();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     public void onEnable() {
@@ -33,22 +37,18 @@ public class Hugs extends JavaPlugin implements Listener {
     public void onDisable() {
         lastHugTime.clear();
         HandlerList.unregisterAll((Listener) this);
+        executor.shutdownNow();
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onInteract(PlayerInteractEntityEvent e) {
-        Player player = e.getPlayer();
-        Entity entity = e.getRightClicked();
+        var player = e.getPlayer();
+        var entity = e.getRightClicked();
+        var hand = e.getHand();
 
-        if (!e.getHand().equals(EquipmentSlot.HAND) || !player.isSneaking() || !(entity instanceof LivingEntity)) {
-            return;
+        if (hand == EquipmentSlot.HAND && player.isSneaking() && entity instanceof LivingEntity) {
+            executor.submit(() -> processRightClick(player, entity));
         }
-
-        if (!player.hasPermission("hugs.hug")) {
-            return;
-        }
-
-        hug(player, entity);
     }
 
     @EventHandler
@@ -63,27 +63,35 @@ public class Hugs extends JavaPlugin implements Listener {
             return true;
         }
 
-        if (!sender.hasPermission("hugs.command")) {
-            sender.sendMessage(Messages.NO_PERMISSION);
-            return true;
+        executor.submit(() -> processCommand((Player) sender, args));
+        return true;
+    }
+
+    private void processRightClick(@NotNull Player player, @NotNull Entity target) {
+        if (player.hasPermission("hugs.hug")) {
+            hug(player, target);
+        }
+    }
+
+    private void processCommand(@NotNull Player player, @NotNull String[] args) {
+        if (!player.hasPermission("hugs.command")) {
+            player.sendMessage(Messages.NO_PERMISSION);
+            return;
         }
 
         if (args.length == 0) {
-            sender.sendMessage(Messages.COMMAND_USAGE);
-            return true;
+            player.sendMessage(Messages.COMMAND_USAGE);
+            return;
         }
 
-        Player target = getServer().getPlayer(args[0]);
+        var target = getServer().getPlayer(args[0]);
 
         if (target == null) {
-            sender.sendMessage(Messages.PLAYER_NOT_FOUND);
-            return true;
+            player.sendMessage(Messages.PLAYER_NOT_FOUND);
+            return;
         }
 
-        Player player = (Player) sender;
-
         hug(player, target);
-        return true;
     }
 
     private void hug(Player player, Entity entity) {
